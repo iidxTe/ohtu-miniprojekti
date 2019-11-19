34cbfd2f5cd7139f4304be921379206efdabfbf5
bensku <bmi@iki.fi>
2019-11-19 15:04:04 +0200

    Spring init

---
+++ src/main/java/io/github/iidxTe/ohtu/OhtuApplication.java
@@ -0,0 +1,13 @@
+package io.github.iidxTe.ohtu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhtuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhtuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/iidxTe/ohtu/OhtuApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.iidxTe.ohtu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhtuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
