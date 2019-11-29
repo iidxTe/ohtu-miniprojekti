8480bcdbaef9408f9463fc18bef19768b129f0dc
NooraVino <noora.virolainen@yahoo.com>
2019-11-29 14:58:07 +0200

    valid new username?

--- src/main/java/io/github/iidxTe/ohtu/controllers/UserController.java
+++ src/main/java/io/github/iidxTe/ohtu/controllers/UserController.java
@@ -18,24 +18,25 @@ public class UserController {
 
     @Autowired
     private UserDao dao;
-    
+
     @GetMapping("/register")
     public String registerForm(Model model) {
         return "register";
     }
-    
+
     @GetMapping("/login")
     public String loginForm(Model model) {
         return "login";
     }
-    
+
     @PostMapping("/register")
     public String register(@RequestParam String username, @RequestParam String password) {
-        dao.createUser(username, password);
+        if (dao.getUser(username) == null) {
+            dao.createUser(username, password);
+        }
         return "index";
     }
-    
+
     // We're processing login attempts with tools provided by Spring
-    
     // TODO user settings
 }
