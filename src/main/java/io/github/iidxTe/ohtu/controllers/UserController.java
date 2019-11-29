729a85f3dc3a07f9bd4aab0c645ed30e9eebd2bd
NooraVino <noora.virolainen@yahoo.com>
2019-11-29 14:42:03 +0200

    form and controller for registration

--- src/main/java/io/github/iidxTe/ohtu/controllers/UserController.java
+++ src/main/java/io/github/iidxTe/ohtu/controllers/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import io.github.iidxTe.ohtu.dao.UserDao;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Registration, logging in/out and user settings.
@@ -25,6 +27,12 @@ public class UserController {
     @GetMapping("/login")
     public String loginForm(Model model) {
         return "login";
+    }
+    
+    @PostMapping("/register")
+    public String register(@RequestParam String username, @RequestParam String password) {
+        dao.createUser(username, password);
+        return "index";
     }
     
     // We're processing login attempts with tools provided by Spring
