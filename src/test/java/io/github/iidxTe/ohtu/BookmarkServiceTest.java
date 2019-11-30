b9e13673d3c976ab839e6c665b2f458c5f573001
Robert <robert.j.rantanen@helsinki.fi>
2019-11-30 18:08:40 +0200

    fixed broken tests

--- src/test/java/io/github/iidxTe/ohtu/BookmarkServiceTest.java
+++ src/test/java/io/github/iidxTe/ohtu/BookmarkServiceTest.java
@@ -1,7 +1,9 @@
 package io.github.iidxTe.ohtu;
 
+import io.github.iidxTe.ohtu.dao.ListDao;
 import io.github.iidxTe.ohtu.domain.BookmarkService;
 import io.github.iidxTe.ohtu.model.Book;
+import io.github.iidxTe.ohtu.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,9 +11,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BookmarkServiceTest {
 
     private BookmarkService service;
+    private User user;
 
     public BookmarkServiceTest() {
         service = new BookmarkService();
+        service.setDao(new ListDao());
+        user = new User("test");
+        user.setId(0);
     }
 
     @BeforeEach
@@ -20,31 +26,31 @@ public class BookmarkServiceTest {
 
     @Test
     public void listAllWithEmptyListReturnsEmpty() {
-        assertEquals(0, service.listAll().size());
-        assertEquals("[]", service.listAll().toString());
+        assertEquals(0, service.listAll(user).size());
+        assertEquals("[]", service.listAll(user).toString());
     }
 
     @Test
     public void creatingItemIncreasesListAllSize() {
-        service.createBook("abc", "matti", "123");
-        assertEquals(1, service.listAll().size());
+        service.createBook(user, "abc", "matti", "123");
+        assertEquals(1, service.listAll(user).size());
     }
 
     @Test
     public void listAllWithOneItemAddedReturnsThatItem() {
         Book book = new Book("abc", "matti", "123");
-        service.createBook("abc", "matti", "123");
-        assertEquals(book.toString(), service.listAll().get(0).toString());
+        service.createBook(user, "abc", "matti", "123");
+        assertEquals(book.toString(), service.listAll(user).get(0).toString());
     }
 
     @Test
     public void listAllWithMultipleItemsAddedReturnsThoseItems() {
         Book book = new Book("abc", "matti", "123");
         Book book2 = new Book("ihmemaa", "liisa", "456");
-        service.createBook("abc", "matti", "123");
-        service.createBook("ihmemaa", "liisa", "456");
-        assertEquals(book.toString(), service.listAll().get(0).toString());
-        assertEquals(book2.toString(), service.listAll().get(1).toString());
+        service.createBook(user, "abc", "matti", "123");
+        service.createBook(user, "ihmemaa", "liisa", "456");
+        assertEquals(book.toString(), service.listAll(user).get(0).toString());
+        assertEquals(book2.toString(), service.listAll(user).get(1).toString());
     }
 
 }
--- src/test/java/io/github/iidxTe/ohtu/cucumber/Stepdefs.java
+++ src/test/java/io/github/iidxTe/ohtu/cucumber/Stepdefs.java
@@ -8,6 +8,7 @@ import io.github.iidxTe.ohtu.dao.ListDao;
 import io.github.iidxTe.ohtu.domain.BookmarkService;
 import io.github.iidxTe.ohtu.model.Book;
 import io.github.iidxTe.ohtu.model.Bookmark;
+import io.github.iidxTe.ohtu.model.User;
 
 import static org.junit.Assert.*;
 
@@ -15,9 +16,13 @@ public class Stepdefs {
 
     BookmarkService service;
     Book book;
+    User user;
 
     public Stepdefs() {
         service = new BookmarkService();
+        service.setDao(new ListDao());
+        user = new User("test");
+        user.setId(0);
     }
     
     @Given ("the list is empty")
@@ -28,23 +33,23 @@ public class Stepdefs {
     @Given ("the list is not empty")
     public void theListIsNotEmpty() {
         service.setDao(new ListDao());
-        service.createBook("Eeva Virtanen", "Tosi hyvä kirja", "13579");
+        service.createBook(user, "Eeva Virtanen", "Tosi hyvä kirja", "13579");
     }
 
     @When("a book is added")
     public void aBookIsAdded() {
-        service.createBook("Samuli Turska", "Näin kalastat kalaa", "314666");
+        service.createBook(user, "Samuli Turska", "Näin kalastat kalaa", "314666");
     }
 
     @And("another book is added")
     public void anotherBookIsAdded() {
-        service.createBook("Katariina Suuri", "Näin hallitset maailmaa", "9999999");
+        service.createBook(user, "Katariina Suuri", "Näin hallitset maailmaa", "9999999");
     }
 
     @Then("the book should be on the list")
     public void theBookShouldBeOnTheList() {
         boolean found = false;
-        for (Bookmark bookmark : service.listAll()) {
+        for (Bookmark bookmark : service.listAll(user)) {
             Book current = ((Book) bookmark);
             if (current.getIsbn().equals("314666")) {
                 found = true;
@@ -58,7 +63,7 @@ public class Stepdefs {
     public void theBooksShouldBeOnTheList() {
         boolean found1 = false;
         boolean found2 = false;
-        for (Bookmark bookmark : service.listAll()) {
+        for (Bookmark bookmark : service.listAll(user)) {
             Book current = ((Book) bookmark);
             if (current.getIsbn().equals("314666")) {
                 found1 = true;
