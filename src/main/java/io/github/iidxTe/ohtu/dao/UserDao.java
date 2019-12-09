ef4c42133b2c2d67f65071faee4b639277b5e440
Jesse Niininen <findarkside@gmail.com>
2019-12-09 19:40:17 +0200

    Fix mixed tabs and spaces

--- src/main/java/io/github/iidxTe/ohtu/dao/UserDao.java
+++ src/main/java/io/github/iidxTe/ohtu/dao/UserDao.java
@@ -6,31 +6,31 @@ import io.github.iidxTe.ohtu.model.User;
 
 public interface UserDao {
 
-	/**
-	 * Gets or creates an user with given name.
-	 * @param name User name.
-	 * @return An user.
-	 */
-	User getUser(String name);
-	
-	/**
-	 * Gets all users that are in given group.
-	 * @param group Group name.
-	 * @return List of users, possibly empty.
-	 */
-	List<User> getUsersByGroup(String group);
-	
-	/**
-	 * Creates a new user.
-	 * @param name User name.
-	 * @param password Password.
-	 * @return A newly created user.
-	 */
-	User createUser(String name, String password);
-	
-	/**
-	 * Stores update to data of given user.
-	 * @param user User that has been updated.
-	 */
-	void updateUser(User user);
+    /**
+     * Gets or creates an user with given name.
+     * @param name User name.
+     * @return An user.
+     */
+    User getUser(String name);
+
+    /**
+     * Gets all users that are in given group.
+     * @param group Group name.
+     * @return List of users, possibly empty.
+     */
+    List<User> getUsersByGroup(String group);
+
+    /**
+     * Creates a new user.
+     * @param name User name.
+     * @param password Password.
+     * @return A newly created user.
+     */
+    User createUser(String name, String password);
+
+    /**
+     * Stores update to data of given user.
+     * @param user User that has been updated.
+     */
+    void updateUser(User user);
 }
--- src/main/java/io/github/iidxTe/ohtu/model/User.java
+++ src/main/java/io/github/iidxTe/ohtu/model/User.java
@@ -6,53 +6,53 @@ package io.github.iidxTe.ohtu.model;
  */
 public class User {
 
-	/**
-	 * Unique id of the user.
-	 */
-	private int id;
-	
-	/**
-	 * Username set on registration. Cannot be changed.
-	 */
-	private final String name;
-	
-	/**
-	 * Display name set by the user. Should be set to {@link #name} initially.
-	 */
-	private String displayName;
-        
+    /**
+     * Unique id of the user.
+     */
+    private int id;
+
+    /**
+     * Username set on registration. Cannot be changed.
+     */
+    private final String name;
+
+    /**
+     * Display name set by the user. Should be set to {@link #name} initially.
+     */
+    private String displayName;
+
     private String group;
-	
-	public User(String name) {
-		this.name = name;
-	}
 
-	public int getId() {
-		return id;
-	}
+    public User(String name) {
+        this.name = name;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	public String getName() {
-		return name;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public String getDisplayName() {
-		return displayName;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setDisplayName(String displayName) {
-		this.displayName = displayName;
-	}
-        
+    public String getName() {
+        return name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
     public String getGroup() {
         return group;
     }
-        
+
     public void setGroup(String group) {
-		this.group = group;
-	}
-	
+        this.group = group;
+    }
+
 }
